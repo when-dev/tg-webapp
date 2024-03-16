fe35b97c2bc65ddb5aafc0b9373a05edcce7a5d4
Egor Gukov <108114245+when-dev@users.noreply.github.com>
2024-03-16 20:17:35 +0300

    add: redesign prelaoder Spinner

--- src/compoments/Spinner.tsx
+++ src/compoments/Spinner.tsx
@@ -23,35 +23,92 @@ export type SpinnerProps = {
 const Spinner: React.FC<React.PropsWithChildren<SpinnerProps>> = memo(
   ({ className = '', size = 'md', variant = 'primary' }: SpinnerProps) => {
     return (
-      <div className={styles.spinnerContainer}>
-        <svg
-          className={clsx(
-            'animate-spin',
-            sizes[size],
-            variants[variant],
-            className,
-          )}
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          data-testid="loading"
+      <div className={styles.loaderContainer}>
+        <div
+          className={styles.tvdd}
+          role="img"
+          aria-label="Three intersecting rings of twelve pulsing dots that never collide"
         >
-          <circle
-            className="opacity-25"
-            cx="12"
-            cy="12"
-            r="10"
-            stroke="currentColor"
-            strokeWidth="4"
-          ></circle>
-          <path
-            className="opacity-75"
-            fill="currentColor"
-            d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-          ></path>
-        </svg>
-        <span className="sr-only">Loading</span>
+          <div className={styles.tvdd__ring}>
+            <div className={styles.tvdd__ringDots}>
+              <div className={styles.tvdd__ringDot}></div>
+              <div className={styles.tvdd__ringDot}></div>
+              <div className={styles.tvdd__ringDot}></div>
+              <div className={styles.tvdd__ringDot}></div>
+              <div className={styles.tvdd__ringDot}></div>
+              <div className={styles.tvdd__ringDot}></div>
+              <div className={styles.tvdd__ringDot}></div>
+              <div className={styles.tvdd__ringDot}></div>
+              <div className={styles.tvdd__ringDot}></div>
+              <div className={styles.tvdd__ringDot}></div>
+              <div className={styles.tvdd__ringDot}></div>
+              <div className={styles.tvdd__ringDot}></div>
+            </div>
+          </div>
+          <div className={styles.tvdd__ring}>
+            <div className={styles.tvdd__ringDots}>
+              <div className={styles.tvdd__ringDot}></div>
+              <div className={styles.tvdd__ringDot}></div>
+              <div className={styles.tvdd__ringDot}></div>
+              <div className={styles.tvdd__ringDot}></div>
+              <div className={styles.tvdd__ringDot}></div>
+              <div className={styles.tvdd__ringDot}></div>
+              <div className={styles.tvdd__ringDot}></div>
+              <div className={styles.tvdd__ringDot}></div>
+              <div className={styles.tvdd__ringDot}></div>
+              <div className={styles.tvdd__ringDot}></div>
+              <div className={styles.tvdd__ringDot}></div>
+              <div className={styles.tvdd__ringDot}></div>
+            </div>
+          </div>
+          <div className={styles.tvdd__ring}>
+            <div className={styles.tvdd__ringDots}>
+              <div className={styles.tvdd__ringDot}></div>
+              <div className={styles.tvdd__ringDot}></div>
+              <div className={styles.tvdd__ringDot}></div>
+              <div className={styles.tvdd__ringDot}></div>
+              <div className={styles.tvdd__ringDot}></div>
+              <div className={styles.tvdd__ringDot}></div>
+              <div className={styles.tvdd__ringDot}></div>
+              <div className={styles.tvdd__ringDot}></div>
+              <div className={styles.tvdd__ringDot}></div>
+              <div className={styles.tvdd__ringDot}></div>
+              <div className={styles.tvdd__ringDot}></div>
+              <div className={styles.tvdd__ringDot}></div>
+            </div>
+          </div>
+        </div>
       </div>
+
+      // <div className={styles.spinnerContainer}>
+      //   <svg
+      //     className={clsx(
+      //       'animate-spin',
+      //       sizes[size],
+      //       variants[variant],
+      //       className,
+      //     )}
+      //     xmlns="http://www.w3.org/2000/svg"
+      //     fill="none"
+      //     viewBox="0 0 24 24"
+      //     data-testid="loading"
+      //   >
+      //     <circle
+      //       className="opacity-25"
+      //       cx="12"
+      //       cy="12"
+      //       r="10"
+      //       stroke="currentColor"
+      //       strokeWidth="4"
+      //     ></circle>
+      //     <path
+      //       className="opacity-75"
+      //       fill="currentColor"
+      //       d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+      //     ></path>
+      //   </svg>
+      //   {/* <span className="sr-only">Loading</span> */}
+      // </div>
     )
   },
   () => true,
