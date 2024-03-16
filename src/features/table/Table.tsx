3c39794cf688dce6631fd599bf28d6741e8bea6f
Egor Gukov <108114245+when-dev@users.noreply.github.com>
2024-03-16 21:04:18 +0300

    add: logic for table outline

--- src/features/table/Table.tsx
+++ src/features/table/Table.tsx
@@ -1,48 +1,56 @@
-import React from "react";
-import styles from './styles.module.scss';
-import { TableData } from "../../shared/types";
-import { twoPersonTable } from "../../shared/assets";
-import { tableCapacityCases } from "../../shared/constants";
+import React, { useState } from 'react'
+import styles from './styles.module.scss'
+import { TableData } from '../../shared/types'
+import { twoPersonTable } from '../../shared/assets'
+import { tableCapacityCases } from '../../shared/constants'
 
 type Props = {
-    info: TableData,
+  info: TableData
+  isSelected: boolean
+  onClick: () => void
 }
-const Table = ({ info }: Props) => {
-    const { id, capacity, location, status } = info;
+const Table = ({ info, isSelected, onClick }: Props) => {
+  const { id, capacity, location, status } = info
 
-    const getAvailabilityColor = () => {
-        switch (status) {
-            case 'free':
-                return 'rgba(0, 211, 34, 1)';
-            case 'reserved':
-                return 'orange';
-            case 'occupied':
-                return 'red';
-        }
-    };
+  const getAvailabilityColor = () => {
+    switch (status) {
+      case 'free':
+        return 'rgba(0, 211, 34, 1)'
+      case 'reserved':
+        return 'orange'
+      case 'occupied':
+        return 'red'
+    }
+  }
 
-    return (
-        <div className={styles.root}>
-            <div className={styles.main}>
-                <div className={styles.tableInfo}>
-                    <div className={styles.tableInfoTitle}>
-                        <div 
-                            className={styles.availability} 
-                            style={{backgroundColor: getAvailabilityColor()}}
-                        />
-                        <h1>{`Стол №${id}`.toLocaleUpperCase()}</h1>
-                    </div>
-                    <div className={styles.tableDetails}>
-                        <p>{`Стол на ${capacity} ${tableCapacityCases.get(capacity)}`}<span>|</span>{`${location} этаж`}</p>
-                    </div>
-                </div>
-                <div className={styles.table}>
-                    <img src={twoPersonTable} />
-                </div>
-            </div>
-
+  return (
+    <div className={styles.root}>
+      <div
+        className={`${styles.main} ${isSelected ? styles.selected : ''}`}
+        onClick={onClick}
+      >
+        <div className={styles.tableInfo}>
+          <div className={styles.tableInfoTitle}>
+            <div
+              className={styles.availability}
+              style={{ backgroundColor: getAvailabilityColor() }}
+            />
+            <h1>{`Стол №${id}`.toLocaleUpperCase()}</h1>
+          </div>
+          <div className={styles.tableDetails}>
+            <p>
+              {`Стол на ${capacity} ${tableCapacityCases.get(capacity)}`}
+              <span>|</span>
+              {`${location} этаж`}
+            </p>
+          </div>
         </div>
-    )
-};
+        <div className={styles.table}>
+          <img src={twoPersonTable} />
+        </div>
+      </div>
+    </div>
+  )
+}
 
-export default Table;
+export default Table
--- src/features/tableList/TableList.tsx
+++ src/features/tableList/TableList.tsx
@@ -1,22 +1,34 @@
-import React from "react"
-import { TableData } from "../../shared/types";
-import Table from "../table";
-import { mockAvailableTables } from "../../shared/constants";
-import styles from './styles.module.scss';
-import { List } from "@mui/material";
+import React, { useState } from 'react'
+import { TableData } from '../../shared/types'
+import Table from '../table'
+import { mockAvailableTables } from '../../shared/constants'
+import styles from './styles.module.scss'
+import { List } from '@mui/material'
 
 const TableList = () => {
-    const availableTables: TableData[] = mockAvailableTables; // useSelector(selectAvailableTables)
+  const availableTables: TableData[] = mockAvailableTables // useSelector(selectAvailableTables)
+  const [selectedTableId, setSelectedTableId] = useState<number | null>(null)
 
-    return (
-        <div className={styles.root}>
-            <List style={{maxHeight: '100%', overflow: 'auto'}}>
-                {availableTables.map((table, id) => (
-                    <Table key={id} info={table} />
-                ))}
-            </List>
-        </div>
+  const handleTableClick = (tableId: number) => {
+    setSelectedTableId((prevSelectedTableId) =>
+      prevSelectedTableId === tableId ? null : tableId,
     )
-};
+  }
 
-export default TableList;
+  return (
+    <div className={styles.root}>
+      <List style={{ maxHeight: '100%', overflow: 'auto' }}>
+        {availableTables.map((table) => (
+          <Table
+            key={table.id}
+            info={table}
+            isSelected={table.id === selectedTableId}
+            onClick={() => handleTableClick(table.id)}
+          />
+        ))}
+      </List>
+    </div>
+  )
+}
+
+export default TableList
